5a200a7f70a75eff03526abba1a74278292188f1
Nikhil Angad Bakshi <bakshienator77@gmail.com>
2018-11-13 08:39:16 +0000

    set dense layer as 1024

--- model.py
+++ model.py
@@ -107,7 +107,7 @@ def cnn(features, labels, mode):
     # Densely connected layer with 1024 neurons
     # Input Tensor Shape: [batch_size, 16 * 16 * 64]
     # Output Tensor Shape: [batch_size, 1024]
-    dense = tf.layers.dense(inputs=pool2_flat, units=512, activation=tf.nn.relu)
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
 
     # Add dropout operation; 0.6 probability that element will be kept
     dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
